cb14a50ab9cd2b4c8af0ac4366211c05f7e04d31
Brett Morriss <brettmorriss@gmail.com>
2023-10-06 11:10:38 -1000

    Add ViewTransitions to BaseHead in application

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap()]
 });
+
+/* viewTransitions: true */
