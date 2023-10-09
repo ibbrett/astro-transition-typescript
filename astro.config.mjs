a5318cb80f14e686e2a8332de7eff609341bb0c7
Brett Morriss <brettmorriss@gmail.com>
2023-10-09 11:27:32 -1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ibbrett.github.io',
+	base: '/astro-transition-typescript',
 	integrations: [mdx(), sitemap()]
 });
 
