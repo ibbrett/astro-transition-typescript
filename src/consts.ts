6d2e0d47095a6ac2f57a7a850c4f5dcd2d0b67d7
Brett Morriss <brettmorriss@gmail.com>
2023-10-12 20:38:20 -1000

    Update SEO

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "ECMAScript Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE =
+  "The Complete Evolution of JavaScript ECMAScript: ES5, ES6 and beyond";
+export const SITE_DESCRIPTION =
+  "Netscape Microsoft JavaScript, JS, Typescript, JScript ECMAScript, ECMA-262. ES5, ES6, ECMAScript 2015, let, const, arrow functions, JSON, promises, async";
