9fb67e55151bc7fad1ff23aebdbdfc96670b1415
Brett Morriss <brettmorriss@gmail.com>
2023-10-12 21:00:49 -1000

    Update header title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const SITE_HEADER_TITLE = "ECMAScript Evolution";
 export const SITE_TITLE =
   "The Complete Evolution of JavaScript ECMAScript: ES5, ES6 and beyond";
 export const SITE_DESCRIPTION =
