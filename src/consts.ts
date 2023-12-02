d6567e35b3d6637ca66b095643f8ca10179a8e94
Brett Morriss <brettmorriss@gmail.com>
2023-12-02 10:37:43 -1000

    Update home, about content

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_HEADER_TITLE = "ECMAScript Evolution";
+export const SITE_HEADER_TITLE = "ECMABlog"; // "ECMAScript Evolution";
 export const SITE_TITLE =
-  "The Complete Evolution of JavaScript ECMAScript: ES5, ES6 and beyond";
+  "The Evolution of JavaScript ECMAScript: ES5, ES6 and beyond";
 export const SITE_DESCRIPTION =
   "Netscape Microsoft JavaScript, JS, Typescript, JScript ECMAScript, ECMA-262. ES5, ES6, ECMAScript 2015, let, const, arrow functions, JSON, promises, async";
